990b24884e6658bd024d6caae6548867ee4bdbe5
Art Gillespie <art@tapsquare.com>
2018-01-14 21:56:20 -0800

    extra: practicing slices

---
+++ ch4/extra/extra_test.go
@@ -0,0 +1,54 @@
+package extra_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+type IntStack []int
+
+func (s *IntStack) Push(i int) {
+	if len(*s) == 0 {
+		*s = []int{i}
+		return
+	}
+	*s = append([]int{i}, *s...)
+}
+
+func (s *IntStack) Pop() (int, error) {
+	if len(*s) == 0 {
+		return 0, fmt.Errorf("IntStack length == 0")
+	}
+	r := (*s)[0]
+	if len(*s) == 1 {
+		*s = make([]int, 0)
+	} else {
+		*s = (*s)[1:]
+	}
+	return r, nil
+}
+
+func TestIntStack(t *testing.T) {
+	var s IntStack
+	s.Push(0)
+	s.Push(1)
+	s.Push(2)
+	if v, err := s.Pop(); err != nil {
+		t.Errorf("Pop() error %v", err)
+	} else if v != 2 {
+		t.Errorf("Expected 2, got %d", v)
+	}
+	if v, err := s.Pop(); err != nil {
+		t.Errorf("Pop() error %v", err)
+	} else if v != 1 {
+		t.Errorf("Expected 1, got %d", v)
+	}
+	if v, err := s.Pop(); err != nil {
+		t.Errorf("Pop() error %v", err)
+	} else if v != 0 {
+		t.Errorf("Expected 0, got %d, %v", v, s)
+	}
+	if l := len(s); l != 0 {
+		t.Errorf("Expected len == 0, got %d, %v", l, s)
+	}
+}
