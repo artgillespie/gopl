445ee74c6bf24e0102a1d87e0f1cf3b6915ba634
Art Gillespie <art@tapsquare.com>
2018-01-15 12:34:38 -0800

    Fixed!

--- ch4/ex7/ex7_test.go
+++ ch4/ex7/ex7_test.go
@@ -1,7 +1,6 @@
 package ex7_test
 
 import (
-	"log"
 	"testing"
 	"unicode/utf8"
 )
@@ -11,16 +10,14 @@ import (
 func Reverse(b []byte) {
 	// DecodeRune -> use width to copy to b2[j-w:j]
 	b2 := make([]byte, len(b))
-	log.Printf("b2: %d\n", len(b2))
 	for i, w := 0, 0; i < len(b); i += w {
 		_, width := utf8.DecodeRune(b[i:])
 		y := len(b2) - (width) - i
 		z := y + (width)
-		log.Printf("y: %d, %d, %d, %d\n", y, z, i, width)
-		copy(b2[y:z], b[i:width])
+		copy(b2[y:z], b[i:i+width])
 		w = width
 	}
-	copy(b, b2)
+	copy(b[:], b2[:])
 }
 
 func RuneAtIndex(b []byte, idx int) rune {
@@ -35,7 +32,7 @@ func RuneAtIndex(b []byte, idx int) rune {
 }
 
 func TestReverse(t *testing.T) {
-	b := []byte("ø∫")
+	b := []byte("ø∫™ªº¡")
 	t.Logf("%s", b)
 	Reverse(b)
 	t.Logf("%s", b)
