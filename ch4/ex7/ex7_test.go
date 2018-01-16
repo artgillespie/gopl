97db72a8c574a2d8943d3792db067533907a1278
Art Gillespie <art@tapsquare.com>
2018-01-16 05:47:21 -0800

    ex4.7 with no allocations

--- ch4/ex7/ex7_test.go
+++ ch4/ex7/ex7_test.go
@@ -1,23 +1,37 @@
 package ex7_test
 
 import (
+	"bytes"
 	"testing"
 	"unicode/utf8"
 )
 
 // Reverse ex4.7 — Modify reverse to reverse the characters of a []byte slice that
 // represents a UTF-8-encoded string, in place. Can you do it without allocating new memory?
-func Reverse(b []byte) {
-	// DecodeRune -> use width to copy to b2[j-w:j]
-	b2 := make([]byte, len(b))
+//                   Reverse Runes     Reverse entire buffer
+// [[123], [456]] -> [[321], [654]] -> [456, 123]
+func ReverseUTF8(b []byte) {
 	for i, w := 0, 0; i < len(b); i += w {
 		_, width := utf8.DecodeRune(b[i:])
-		y := len(b2) - (width) - i
-		z := y + (width)
-		copy(b2[y:z], b[i:i+width])
+		Reverse(b[i : i+width])
 		w = width
 	}
-	copy(b[:], b2[:])
+	Reverse(b)
+}
+
+func Reverse(b []byte) {
+	for i, j := 0, len(b)-1; i < j; i++ {
+		b[i], b[j] = b[j], b[i]
+		j--
+	}
+}
+
+func TestReverse(t *testing.T) {
+	b := []byte("Hello")
+	Reverse(b)
+	if bytes.Compare(b, []byte{'o', 'l', 'l', 'e', 'H'}) != 0 {
+		t.Errorf("Expected 'olleH', got %s", b)
+	}
 }
 
 func RuneAtIndex(b []byte, idx int) rune {
@@ -31,10 +45,11 @@ func RuneAtIndex(b []byte, idx int) rune {
 	return ' '
 }
 
-func TestReverse(t *testing.T) {
+func TestReverseUTF8(t *testing.T) {
 	b := []byte("ø∫™ªº¡")
-	t.Logf("%s", b)
-	Reverse(b)
-	t.Logf("%s", b)
-	//t.Logf("Rune: %v", RuneAtIndex([]byte("ø™ø"), 1))
+	ReverseUTF8(b)
+	e := "¡ºª™∫ø"
+	if bytes.Compare([]byte(e), b) != 0 {
+		t.Errorf("Expected %s, got %s", e, b)
+	}
 }
