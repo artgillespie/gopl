7e140fb89d260b6c53f0bc491870fb6134a44b9d
Art Gillespie <art@udacity.com>
2018-01-21 15:47:05 -0800

    ex 4.9 w/sorting

--- ch4/ex9/wordfreq.go
+++ ch4/ex9/wordfreq.go
@@ -4,7 +4,20 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
+
+// sort by frequency
+type WordFreq struct {
+	Word  string
+	Count int
+}
+
+type WordFreqSlice []WordFreq
+
+func (a WordFreqSlice) Len() int           { return len(a) }
+func (a WordFreqSlice) Less(i, j int) bool { return a[i].Count > a[j].Count }
+func (a WordFreqSlice) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 
 func main() {
 	count := make(map[string]int)
@@ -19,8 +32,14 @@ func main() {
 			count[word]++
 		}
 	}
-	fmt.Println("Word\tCount")
+
+	// sort in descending order by number of instances
+	var wordFreqs WordFreqSlice
 	for k, v := range count {
-		fmt.Printf("%s\t%d\n", k, v)
+		wordFreqs = append(wordFreqs, WordFreq{k, v})
+	}
+	sort.Sort(wordFreqs)
+	for _, f := range wordFreqs {
+		fmt.Printf("%s\t%d\n", f.Word, f.Count)
 	}
 }
