d34e135cde13df3fef79ebbf718595ed307a9b69
Art Gillespie <art@tapsquare.com>
2018-01-28 18:59:03 -0800

    ex4.11 Write function for opening user's text editor.

---
+++ ch4/ex11/issues/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+)
+
+func editWithUsersTextEditor(s string) (string, error) {
+	// write out the string we want the user to edit to a tmp file
+	f, err := os.OpenFile("/tmp/issues.txt", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return "", err
+	}
+	_, err = f.WriteString(s)
+	f.Close()
+	if err != nil {
+		return "", err
+	}
+
+	// get the editor, fail if the env var isn't set
+	editor, ok := os.LookupEnv("EDITOR")
+	if !ok {
+		return "", fmt.Errorf("No EDITOR environment variable set")
+	}
+	// get the full path to the user's text editor
+	fullEditorPath, err := exec.LookPath(editor)
+	if err != nil {
+		return "", err
+	}
+
+	// launch the user's editor
+	cmd := exec.Command(fullEditorPath, "/tmp/issues.txt")
+	// even if we're not using these, they have to be connected in order for this to work
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		return "", err
+	}
+	// open the tmp file and read the user's edits back in
+	f, err = os.Open("/tmp/issues.txt")
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func main() {
+	s, err := editWithUsersTextEditor("Hello, Text Editor!")
+	if err != nil {
+		panic(fmt.Sprintf("Couldn't edit with text editor: %s", err))
+	}
+	fmt.Println("After Edit:")
+	fmt.Println(s)
+}
